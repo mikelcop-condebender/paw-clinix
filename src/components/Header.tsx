eec093d583d6901853651b053feea316cc2f17a6
maebelle29 <ladridomebel@gmail.com>
2024-11-09 12:41:44 +0800

    revert clinix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Menu } from "./Menu";
 
 const Header: React.FC = () => (
   <header className="bg-[#000] text-white p-4 flex justify-between items-center">
-    <h1 className="text-2xl font-bold">CliniX</h1>
+    <h1 className="text-2xl font-bold">Paw CliniX</h1>
     <Menu />
   </header>
 );
